b97a99f1701f51928f3406b3ce23b69af25de3f5
JohanZhu <634606335@qq.com>
2016-09-25 19:55:12 +0800

    my resume 0.0.1 version

---
+++ js/main.js
@@ -0,0 +1,93 @@
+$(function(){
+	$('.HTMLCSS').next().click(function(){
+		$('.fi').css({"display":"none"})
+		$('.th').css({"display":"none"})
+		$('.re').css({"display":"none"})
+		$('.jq').css({"display":"none"})
+		$('.js').css({"display":"none"})
+		$('.htmlcss').slideDown();
+	})
+	$('.JS').next().click(function(){
+		$('.fi').css({"display":"none"})
+		$('.th').css({"display":"none"})
+		$('.re').css({"display":"none"})
+		$('.jq').css({"display":"none"})
+		$('.js').slideDown();
+		$('.htmlcss').css({"display":"none"})
+	})
+	$('.Jquery').next().click(function(){
+		$('.fi').css({"display":"none"})
+		$('.th').css({"display":"none"})
+		$('.re').css({"display":"none"})
+		$('.jq').slideDown();
+		$('.js').css({"display":"none"})
+		$('.htmlcss').css({"display":"none"})
+	})
+	$('.React').next().click(function(){
+		$('.fi').css({"display":"none"})
+		$('.th').css({"display":"none"})
+		$('.re').slideDown();
+		$('.jq').css({"display":"none"})
+		$('.js').css({"display":"none"})
+		$('.htmlcss').css({"display":"none"})
+	})
+	$('.Three').next().click(function(){
+		$('.fi').css({"display":"none"})
+		$('.th').slideDown();
+		$('.re').css({"display":"none"})
+		$('.jq').css({"display":"none"})
+		$('.js').css({"display":"none"})
+		$('.htmlcss').css({"display":"none"})
+	})
+	$('.fireworks').next().click(function(){
+		$('.fi').slideDown();
+		$('.th').css({"display":"none"})
+		$('.re').css({"display":"none"})
+		$('.jq').css({"display":"none"})
+		$('.js').css({"display":"none"})
+		$('.htmlcss').css({"display":"none"})
+	})
+window.onscroll = function(){
+	    $('.fi').css({"display":"none"})
+		$('.th').css({"display":"none"})
+		$('.re').css({"display":"none"})
+		$('.jq').css({"display":"none"})
+		$('.js').css({"display":"none"})
+		$('.htmlcss').css({"display":"none"})
+}
+window.onload = function(){
+	$('.jiaoyujingli').slideDown(1000);
+}
+$('.topBar').click(function(){
+       var sc=$(window).scrollTop();
+       $('body,html').animate({scrollTop:0},500);	
+})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+})
